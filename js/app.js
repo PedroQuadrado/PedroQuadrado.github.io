e5cec590a7e430f44135ee57d6db0c0a47559482
PedroQuadrado <51176756+PedroQuadrado@users.noreply.github.com>
2020-02-02 22:17:13 +0000

    Buttons added

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var info = document.getElementById('info');
+var skills = document.getElementById('skills');
+var languages = document.getElementById('languages');
+var projects = document.getElementById('projects');
+
+function info(){
+    info.style.display = 'initial'; 
+    skils.style.display = 'none'; 
+    languages.style.display = 'none'; 
+    projects.style.display = 'none'; 
+}
